ce22e7c940094f886b26ddee1a2192aaffd17ab0
shushikata <shu.42111227@gmail.com>
2020-08-04 06:20:04 +0000

    [Finish]comment_ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,18 @@
 class CommentsController < ApplicationController
 
   def create 
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = Comment.new(comment_params)
     comment.user_id = current_user.id 
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
   end
 
 
