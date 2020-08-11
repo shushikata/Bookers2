b20bfd4473767af21fc90334afa4f5a39ea68f9e
shushikata <shu.42111227@gmail.com>
2020-08-11 10:59:34 +0000

    aa

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :books, dependent: :destroy 
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  attachment :profile_image
+
+  validates :name, presence: true, length: { minimum:2, maximum:20 }
+  validates :introduction, length: { maximum:50 }
+
+
 
   # mailer関連 ========================================
   after_create :send_welcome_mail
@@ -12,9 +22,7 @@ class User < ApplicationRecord
   end
   # ===================================================
 
-  has_many :books, dependent: :destroy 
-  has_many :comments, dependent: :destroy
-  has_many :favorites, dependent: :destroy
+ 
 
   # DM関連 ===========================================================================
     has_many :messages
@@ -29,13 +37,6 @@ class User < ApplicationRecord
       end
     end
   # ==================================================================================
-
-  
-  attachment :profile_image
-
-  validates :name, presence: true, length: { minimum:2, maximum:20 }
-  validates :introduction, length: { maximum:50 }
-
   
   
   # ====================自分がフォローしているユーザーとの関連 ===================================
